0ece783aa8df4b6c8e5f1a65a5edadce2498f02a
Jean Hsu <kaichinh@usc.edu>
2021-10-05 00:30:21 -0700

    add basic login page

---
+++ src/main/webapp/loginBox.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const e = React.createElement;
+
+
+const formStyle = {
+    margin: 'auto',
+    padding: '10px',
+    border: '1px solid #c9c9c9',
+    borderRadius: '5px',
+    background: '#f5f5f5',
+    width: '220px',
+    display: 'block'
+};
+
+const labelStyle = {
+    margin: '10px 0 5px 0',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '15px',
+};
+
+const inputStyle = {
+    margin: '5px 0 10px 0',
+    padding: '5px',
+    border: '1px solid #bfbfbf',
+    borderRadius: '3px',
+    boxSizing: 'border-box',
+    width: '100%'
+};
+
+const submitStyle = {
+    margin: '10px 0 0 0',
+    padding: '7px 10px',
+    border: '1px solid #efffff',
+    borderRadius: '3px',
+    background: '#3085d6',
+    width: '100%',
+    fontSize: '15px',
+    color: 'white',
+    display: 'block'
+};
+
+const Field = React.forwardRef(({label, type}, ref) => {
+    return (
+      <div>
+        <label style={labelStyle} >{label}</label>
+        <input ref={ref} type={type} style={inputStyle} />
+      </div>
+    );
+});
+
+const validateData = data => {
+    const json = JSON.stringify(data, null, 4);
+    alert(json);
+
+};
+
+const Form = (props) => {
+    const usernameRef = React.useRef();
+    const passwordRef = React.useRef();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value
+        };
+        validateData(data);
+
+    };
+    return (
+      <form style={formStyle} onSubmit={handleSubmit}  >
+        <Field ref={usernameRef} label="Username:" type="text" />
+        <Field ref={passwordRef} label="Password:" type="password" />
+        <div>
+          <button style={submitStyle} type="submit" >Submit</button>
+        </div>
+      </form>
+    );
+};
+
+
+
+const domContainer2 = document.querySelector('#login_box_container');
+//ReactDOM.render(React.createElement('div', null, 'Hello World'), domContainer2);
+ReactDOM.render(e(Form), domContainer2);
