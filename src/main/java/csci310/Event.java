b442bed892d2c9c3919371e72d5ba825e95a9e76
Leena Mathur <lmathur@usc.edu>
2021-10-13 14:54:28 -0700

    make events have list of user objects as attendees; update the unit tests for addAttendee() and removeAttendee() to account for these new user objects

--- src/main/java/csci310/Event.java
+++ src/main/java/csci310/Event.java
@@ -4,41 +4,45 @@ import java.util.Date;
 import java.util.List;
 
 public class Event {
+    private int id;
     private String title;
     private String description;
     private Date date;
     private String creator;
     private String start;
     private String end;
-    private List<String> attendees = new ArrayList<>();
+    private List<User> attendees = new ArrayList<>();
 
     // getters
-    public String getTitle() {
+    public int getEventID(){ return this.id;}
+    public String getEventTitle() {
         return this.title;
     }
-    public String getDescription() { return this.description;}
-    public Date getDate(){ return this.date; }
-    public String getCreator(){ return this.creator; }
-    public String getStart(){ return this.start;}
-    public String getEnd(){ return this.end;}
-    public List<String> getAttendees() {return this.attendees;}
+    public String getEventDescription() { return this.description;}
+    public Date getEventDate(){ return this.date; }
+    public String getEventCreator(){ return this.creator; }
+    public String getEventStart(){ return this.start;}
+    public String getEventEnd(){ return this.end;}
+    public List<User> getAttendees() {return this.attendees;}
 
     // setters
+    public void setEventID(int newid) { this.id = newid;}
     public void setTitle(String newtitle) {this.title = newtitle;}
     public void setDescription(String newdescription) {this.description = newdescription;}
     public void setCreator(String newcreator) {this.creator = newcreator;}
     public void setStart(String newstart) {this.start = newstart;}
     public void setEnd(String newend) {this.end = newend;}
     public void setDate(Date newdate) {this.date = newdate;}
-    public void setAttendees(List<String> newattendees) {this.attendees = newattendees;}
+    public void setAttendees(List<User> newattendees) {this.attendees = newattendees;}
 
-    // methods
-    public void addAttendee(String attendee_name) {
-        this.attendees.add(attendee_name);
+    // add a user to the list of attendees
+    public void addAttendee(User user_object) {
+        this.attendees.add(user_object);
     }
 
-    public void removeAttendee(String attendee_name) {
-        this.attendees.remove(attendee_name);
+    // remove a user from the list of attendees
+    public void removeAttendee(User user_object) {
+        this.attendees.remove(user_object);
     }
 
 }
