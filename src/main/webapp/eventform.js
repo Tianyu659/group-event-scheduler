6d4ebf312491d3393e9684f75d93043392312b50
Rory Blonski <blonski@usc.edu>
2021-10-20 16:32:51 -0700

    event creation form generally done, need to develop acceptance tests, integrate more logic into the actual page.

--- src/main/webapp/eventform.js
+++ src/main/webapp/eventform.js
@@ -7,6 +7,7 @@ const EventForm = (props) => {
 
     const [creatingUser, setCreatingUser] = useState(''); // username of user that clicked 'create event'
     const [events, setEvents] = useState(data); // list of events
+    const [times, setTimes] = useState([]);
     const [selectedEvent, setSelectedEvent] = useState(0); // id of event. 
     const [selectedTimes, setSelectedTimes] = useState([]);
     const [invitedUsers, setInvitedUsers] = useState([]);
@@ -42,13 +43,21 @@ const EventForm = (props) => {
 
                 <div className="form-section">
                     <span className="form-label">select event</span>
+                    
                     <div> 
                         {events.map((e) => {
+                            let days = "";
+                            for(let i = 0; i < e.days.length; i++) {
+                                days += e.days[i];
+                                days += " ";
+                            }
                             return (
                                 <div className="event-section" key={e.name} id={e.id} onClick={(ev) => setActiveEvent(ev, e.id)}>
                                     <span>{e.name}</span>
                                     <br />
                                     <span>{e.location}</span> 
+                                    <br />
+                                    <span>{days}</span>
                                 </div>
                             );
                         })}
@@ -57,6 +66,7 @@ const EventForm = (props) => {
 
                 <div className="form-section">
                     <span className="form-label">invite users</span>
+                    
                     <div> 
                         <span>User 1: </span>
                         <input type="text"></input>
@@ -78,28 +88,39 @@ const EventForm = (props) => {
 
                 <div className="form-section">
                     <span className="form-label">select times</span>
+                    
                     <div> 
-                        <div>
-                            <div id="a7" className="time-section" onClick={(e) => toggleTime(e, "a", 7)}><span>7:00 AM</span></div>
-                            <div id="a8" className="time-section" onClick={(e) => toggleTime(e, "a", 8)}><span>8:00 AM</span></div>
-                            <div id="a9" className="time-section" onClick={(e) => toggleTime(e, "a", 9)}><span>9:00 AM</span></div>
-                            <div id="a10" className="time-section" onClick={(e) => toggleTime(e, "a", 10)}><span>10:00 AM</span></div>
-                            <div id="a11" className="time-section" onClick={(e) => toggleTime(e, "a", 11)}><span>11:00 AM</span></div>
-                            <div id="a12" className="time-section" onClick={(e) => toggleTime(e, "a", 12)}><span>12:00 PM</span></div>
-                            <div id="a13" className="time-section" onClick={(e) => toggleTime(e, "a", 13)}><span>1:00 PM</span></div>
-                            <div id="a14" className="time-section" onClick={(e) => toggleTime(e, "a", 14)}><span>2:00 PM</span></div>
-                            <div id="a15" className="time-section" onClick={(e) => toggleTime(e, "a", 15)}><span>3:00 PM</span></div>
-                            <div id="a16" className="time-section" onClick={(e) => toggleTime(e, "a", 16)}><span>4:00 PM</span></div>
-                            <div id="a17" className="time-section" onClick={(e) => toggleTime(e, "a", 17)}><span>5:00 PM</span></div>
-                            <div id="a18" className="time-section" onClick={(e) => toggleTime(e, "a", 18)}><span>6:00 PM</span></div>
-                            <div id="a19" className="time-section" onClick={(e) => toggleTime(e, "a", 19)}><span>7:00 PM</span></div>
-                            <div id="a20" className="time-section" onClick={(e) => toggleTime(e, "a", 20)}><span>8:00 PM</span></div>
-                            <div id="a21" className="time-section" onClick={(e) => toggleTime(e, "a", 21)}><span>9:00 PM</span></div>
-                            <div id="a22" className="time-section" onClick={(e) => toggleTime(e, "a", 22)}><span>10:00 PM</span></div>
-                            <div id="a23" className="time-section" onClick={(e) => toggleTime(e, "a", 23)}><span>11:00 PM</span></div>
-                            <div id="a24" className="time-section" onClick={(e) => toggleTime(e, "a", 24)}><span>12:00 PM</span></div>
+                        <div className="time-section-container">
+                            <div className="time-section-holder">
+                                <div id="a7" className="time-section" onClick={(e) => toggleTime(e, "a", 7)}><span>7:00 AM</span></div>
+                                <div id="a8" className="time-section" onClick={(e) => toggleTime(e, "a", 8)}><span>8:00 AM</span></div>
+                                <div id="a9" className="time-section" onClick={(e) => toggleTime(e, "a", 9)}><span>9:00 AM</span></div>
+                                <div id="a10" className="time-section" onClick={(e) => toggleTime(e, "a", 10)}><span>10:00 AM</span></div>
+                                <div id="a11" className="time-section" onClick={(e) => toggleTime(e, "a", 11)}><span>11:00 AM</span></div>
+                                <div id="a12" className="time-section" onClick={(e) => toggleTime(e, "a", 12)}><span>12:00 PM</span></div>
+                            </div>
+                            <div className="time-section-holder">
+                                <div id="a13" className="time-section" onClick={(e) => toggleTime(e, "a", 13)}><span>1:00 PM</span></div>
+                                <div id="a14" className="time-section" onClick={(e) => toggleTime(e, "a", 14)}><span>2:00 PM</span></div>
+                                <div id="a15" className="time-section" onClick={(e) => toggleTime(e, "a", 15)}><span>3:00 PM</span></div>
+                                <div id="a16" className="time-section" onClick={(e) => toggleTime(e, "a", 16)}><span>4:00 PM</span></div>
+                                <div id="a17" className="time-section" onClick={(e) => toggleTime(e, "a", 17)}><span>5:00 PM</span></div>
+                                <div id="a18" className="time-section" onClick={(e) => toggleTime(e, "a", 18)}><span>6:00 PM</span></div>
+                            </div>
+                            <div className="time-section-holder">
+                                <div id="a19" className="time-section" onClick={(e) => toggleTime(e, "a", 19)}><span>7:00 PM</span></div>
+                                <div id="a20" className="time-section" onClick={(e) => toggleTime(e, "a", 20)}><span>8:00 PM</span></div>
+                                <div id="a21" className="time-section" onClick={(e) => toggleTime(e, "a", 21)}><span>9:00 PM</span></div>
+                                <div id="a22" className="time-section" onClick={(e) => toggleTime(e, "a", 22)}><span>10:00 PM</span></div>
+                                <div id="a23" className="time-section" onClick={(e) => toggleTime(e, "a", 23)}><span>11:00 PM</span></div>
+                                <div id="a24" className="time-section" onClick={(e) => toggleTime(e, "a", 24)}><span>12:00 PM</span></div>
+                            </div>
                         </div>
                     </div>
+                </div>
+
+                <div className="form-section">
+                    <button type="button" className="btn btn-primary form-button-submit">Submit</button>
                 </div>
             </div>
 
--- src/main/webapp/eventform.jsp
+++ src/main/webapp/eventform.jsp
@@ -2,6 +2,8 @@
 <html>
     <head>
         <link rel="stylesheet" href="eventform.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     </head>
     <body>
 
@@ -13,6 +15,7 @@
 
         <script src="dummyData.js"></script>
         <script type="text/babel" src="eventform.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     </body>
 </html>
