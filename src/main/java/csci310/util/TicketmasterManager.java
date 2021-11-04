b3458f205a32abbac3124ec93f30a0c550b58dfa
Tianyu Wang <twang659@usc.edu>
2021-11-04 00:16:38 -0700

    TM manager basic search by keyword function

---
+++ src/main/java/csci310/util/TicketmasterManager.java
@@ -0,0 +1,43 @@
+package csci310.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TicketmasterManager {
+
+	private static final String API_KEY = "7ehIR2akFvwVaFCZWcsvsLzcbEZ4ik8D";
+	
+	private static final String ROOT_URL = "https://app.ticketmaster.com/discovery/v2/";
+	
+	//TODO Event Search
+	public static String searchEventByKeyword(String keyword) throws IOException {
+		URL url = new URL(ROOT_URL + "events.json?apikey=" + API_KEY + "&keyword=" + keyword);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	    connection.setRequestMethod("GET");
+	    connection.setRequestProperty("Content-Type", "application/json");
+	    connection.setRequestProperty("Authorization","Token " + API_KEY);
+	    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	    
+	    String lineString = in.readLine();
+	    return lineString;
+	}	
+	
+	//TODO Event Details
+//	public static String getEventDetails(String eventID) throws IOException {
+//		URL url = new URL(ROOT_URL + "events/" + eventID + ".json?apikey=" + API_KEY);
+//		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        connection.setRequestMethod("GET");
+//        connection.setRequestProperty("Content-Type", "application/json");
+//        connection.setRequestProperty("Authorization","Token " + API_KEY);
+//        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        
+//        String lineString = in.readLine();
+//        return lineString;
+//	}
+	
+	//TODO Event Images
+}
