93c4aad5e02a422ac870c02231f27d60f7e51637
AzureTriple <77744272+AzureTriple@users.noreply.github.com>
2021-11-04 18:57:36 -0700

    Removed redundant code

--- src/main/java/csci310/models/User.java
+++ src/main/java/csci310/models/User.java
@@ -6,68 +6,69 @@ import com.j256.ormlite.table.DatabaseTable;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 
+import java.util.Arrays;
+
 @DatabaseTable(tableName = "users")
 public class User {
     @DatabaseField(generatedId = true)
     private int id;
-
+    
     @DatabaseField(unique = true)
     private String username;
-
+    
     @JsonIgnore
     @DatabaseField
     private String password;
-
+    
     @DatabaseField
     private String firstName;
-
+    
     @DatabaseField
     private String lastName;
-
+    
     public int getId() {
         return this.id;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public String getUsername() {
         return this.username;
     }
-
+    
+    interface PasswordOp<T> {
+        T run(Argon2 argon2,char[] bufer);
+    }
+    
+    <T> T passwordOperation(String password,PasswordOp<T> op) {
+        final Argon2 a2 = Argon2Factory.create();
+        final char[] buf = password.toCharArray();
+        try {return op.run(a2,buf);}
+        finally {a2.wipeArray(buf);}
+    }
+    
     public boolean comparePassword(String password) {
-        Argon2 argon2 = Argon2Factory.create();
-        char[] buffer = password.toCharArray();
-        try {
-            return argon2.verify(this.password, buffer);
-        } finally {
-            argon2.wipeArray(buffer);
-        }
+        return passwordOperation(password,(a,b) -> a.verify(this.password,b));
     }
-
+    
     public void setPassword(String password) {
-        Argon2 argon2 = Argon2Factory.create();
-        char[] buffer = password.toCharArray();
-        try {
-            this.password = argon2.hash(10, 65535, 1, buffer);
-        } finally {
-            argon2.wipeArray(buffer);
-        }
+        this.password = passwordOperation(password,(a,b) -> a.hash(10,65535,1,b));
     }
-
+    
     public String getFirstName() {
         return this.firstName;
     }
-
+    
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+    
     public String getLastName() {
         return lastName;
     }
-
+    
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
