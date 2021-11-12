019effab3da28cd861dd04ff85a95c56fbacecff
Tianyu Wang <twang659@usc.edu>
2021-11-12 13:52:19 -0800

    search by eventSearch model

--- src/main/java/csci310/util/TicketmasterManager.java
+++ src/main/java/csci310/util/TicketmasterManager.java
@@ -1,11 +1,13 @@
 package csci310.util;
 
 import csci310.Configuration;
+import csci310.models.EventSearch;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
 //import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -35,6 +37,18 @@ public class TicketmasterManager {
 	//Event Details
 	public static String getEventDetails(String eventID) throws IOException {
 		URL url = new URL(ROOT_URL + "events/" + eventID + ".json?apikey=" + API_KEY);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Authorization","Token " + API_KEY);
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        
+        String lineString = in.readLine();
+        return lineString;
+	}
+	
+	public static String searchEvent(EventSearch event) throws IOException {
+		URL url = new URL(ROOT_URL + "events.json?apikey=" + API_KEY + event.toString());
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Content-Type", "application/json");
