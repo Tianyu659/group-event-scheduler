d48f94a026c14bc31d7a0ece898261e97d21759a
AzureTriple <77744272+AzureTriple@users.noreply.github.com>
2021-12-01 20:35:47 -0800

    Added tests for group date mechanics

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -5,17 +5,17 @@ import csci310.models.UserTest;
 import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -44,13 +44,11 @@ public class StepDefinitions {
     @Then("I click on login in top right corner")
     public void i_click_on_login() {
         waitForElement(By.cssSelector("#navigation>a:last-of-type")).click();
-        pause(BTN_DELAY);
     }
 
     @Then("I click on register")
     public void i_click_on_register() {
         waitForElement(By.cssSelector("#content>div.form>div.buttons>a")).click();
-        pause(BTN_DELAY);
     }
 
     @Then("I enter registration info: {string}, {string}, {string}, {string}, {string}")
@@ -66,7 +64,7 @@ public class StepDefinitions {
     @Then("I click register")
     public void i_click_register() {
         waitForElement(By.tagName("button")).click();
-        pause(BTN_DELAY);
+        pause();
     }
 
     @Then("I log in with {string}, {string}")
@@ -75,7 +73,7 @@ public class StepDefinitions {
         waitForElement(By.id("username")).sendKeys(username);
         waitForElement(By.id("password")).sendKeys(password);
         waitForElement(By.tagName("button")).click();
-        pause(BTN_DELAY);
+        pause();
     }
 
     @Then("I should see login error")
@@ -95,13 +93,13 @@ public class StepDefinitions {
     @Then("I click login")
     public void i_click_login() {
         waitForElement(By.tagName("button")).click();
-        pause(BTN_DELAY);
+        pause();
     }
 
     @Then("I click logout")
     public void i_click_logout() {
         waitForElement(By.id("logout")).click();
-        pause(BTN_DELAY);
+        pause();
     }
     
     @Then("The create user button should be disabled")
@@ -118,8 +116,8 @@ public class StepDefinitions {
             .forEach(e -> e.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE)));
     }
     
-    private static void pause(final long time) {
-        try {Thread.sleep(time);}
+    private static void pause() {
+        try {Thread.sleep(StepDefinitions.BTN_DELAY);}
         catch(final InterruptedException e) {e.printStackTrace();}
     }
     private WebElement waitForElement(final By by) {
@@ -131,7 +129,6 @@ public class StepDefinitions {
     @Then("I click the create event button")
     public void i_click_the_create_event_button() {
         waitForElement(By.cssSelector("#content>div>h2>a")).click();
-        pause(BTN_DELAY);
     }
     @Then("I set the event name to {string}")
     public void i_set_the_event_name_to(final String s) {
@@ -204,7 +201,7 @@ public class StepDefinitions {
         waitForElement(
             By.cssSelector("#content>div.form>div:nth-of-type(3)>div.form>div.group:nth-of-type(5)>button")
         ).click();
-        pause(BTN_DELAY);
+        pause();
     }
     @Then("I click search result {int}")
     public void i_click_search_result(final int i) {
@@ -222,14 +219,14 @@ public class StepDefinitions {
         final WebElement e = waitForElement(
             By.cssSelector("#content>div.form>div:nth-of-type(4)>div.form")
         );
-        e.findElement(By.cssSelector("div.form-group>input")).sendKeys(s);
+        e.findElement(By.cssSelector("div.form-group>input")).sendKeys(Keys.chord(Keys.CONTROL,"a"),s);
         e.findElement(By.cssSelector("ul>li:first-of-type")).click();
     }
     @Then("I click the finalize button")
     public void i_click_the_finalize_button() {
         waitForElement(By.cssSelector("#content>div.form>div:nth-of-type(5)>button"))
             .click();
-        pause(BTN_DELAY);
+        pause();
     }
     @Then("I should see the event name {string}")
     public void i_should_see_the_event_name(final String s) {
@@ -263,10 +260,8 @@ public class StepDefinitions {
     }
     @Then("I click the logo in the top left")
     public void i_click_the_logo_in_the_top_left() {
-        pause(100);
         waitForElement(By.cssSelector("#navigation>a:first-of-type"))
             .click();
-        pause(BTN_DELAY);
     }
     @Then("I should see date with name {string}")
     public void i_should_see_date_with_name(final String s) {
@@ -294,7 +289,7 @@ public class StepDefinitions {
                 .findFirst();
         assertTrue(we.isPresent());
         we.get().click();
-        pause(BTN_DELAY);
+        pause();
     }
     @Then("I should see information for the group date {string}")
     public void i_should_see_information_for_the_group_date(final String s) {
@@ -304,7 +299,7 @@ public class StepDefinitions {
     public void i_decline_the_group_date() {
         waitForElement(By.cssSelector("#content>div:first-of-type>button"))
             .click();
-        pause(BTN_DELAY);
+        pause();
     }
     
     @Then("I should see {int} invites")
@@ -350,6 +345,173 @@ public class StepDefinitions {
         ).forEach(WebElement::click);
     }
     
+    @Then("I register {int} minions")
+    public void i_register_minions(int i) {
+        while(i-- != 0)
+        {
+            i_click_on_register();
+            i_enter_my_registration_info("minion"+i,"asdfjkl1","asdfjkl1","minion_"+i,"helper");
+            i_click_register();
+        }
+    }
+    @Then("I invite {int} minions")
+    public void i_invite_minions(final int i) {
+        for(int j = 0;j < i;++j)
+            i_invite_user("minion_"+j);
+    }
+    @Then("Minion {int} logs in")
+    public void minion_logs_in(final int i) {
+        login("minion"+i,"asdfjkl1");
+    }
+    @Then("The minion clicks the invitation to {string}")
+    public void the_minion_clicks_the_invitation_to(final String s) {
+        i_click_the_invitation_to_the_group_date(s);
+    }
+    @Then("The minion is available for event {int} with interest {int}")
+    public void the_minion_is_available_for_event_with_interest(final int i,final int i1) {
+        final WebElement event = waitForElements(
+            By.cssSelector(
+                "#content>div>div.form>div.event.form>div.group.flex"
+            )
+        ).get(i);
+        new Select(
+            event.findElement(By.tagName("select"))
+        ).selectByVisibleText("Available");
+        event.findElement(By.tagName("input"))
+             .sendKeys(Keys.chord(Keys.CONTROL,"a"),String.valueOf(i1));
+    }
+    @Then("The minion accepts invitation")
+    public void the_minion_accepts_invitation() {
+        waitForElement(
+            By.cssSelector(
+                "#content>div>div.form>button"
+            )
+        ).click();
+        pause();
+    }
+    @Then("The minion logs out")
+    public void the_minion_logs_out() {
+        i_click_logout();
+    }
+    @Then("I click the event {string}")
+    public void i_click_the_event(final String s) {
+        final Optional<WebElement> we = waitForElements(
+            By.cssSelector(
+                "#content>div:first-of-type>ul>li"
+            )
+        ).parallelStream()
+            .filter(e -> e.getText().contentEquals(s))
+            .findFirst();
+        assertTrue(we.isPresent());
+        we.get().click();
+        pause();
+    }
+    private static final Pattern CLS_BEST = Pattern.compile(".*best.*"),
+                                 INTEREST = Pattern.compile(".*(\\d+)/\\d+\\D+((?:\\d*\\.)?\\d+).*");
+    @Then("Event {int} should be highlighted with availability {int} and interest {float}")
+    public void event_should_be_highlighted_with_availability_and_interest(final int i,final int i1,final double d) {
+        final Iterator<WebElement> events = waitForElements(By.className("event")).iterator();
+        for(int j = 0;events.hasNext();++j) {
+            final WebElement e = events.next();
+            assertEquals(i == j,CLS_BEST.matcher(e.getAttribute("class")).matches());
+            if(i == j) {
+                final Matcher m = INTEREST.matcher(
+                    e.findElement(By.cssSelector("p:first-of-type"))
+                        .getText()
+                );
+                assertTrue(m.find());
+                assertEquals(i1,Integer.parseInt(m.group(1)));
+                assertEquals(d,Double.parseDouble(m.group(2)),0.01);
+            }
+        }
+    }
+    @Then("I should see {int} responses")
+    public void i_should_see_responses(final int i) {
+        assertEquals(
+            i,
+            waitForElements(By.className("invitation")).size()
+        );
+    }
+    private void alert(final boolean accept) {
+        final Alert alert = driver.switchTo().alert();
+        if(accept) alert.accept();
+        else alert.dismiss();
+    }
+    private static final Pattern INVITATION = Pattern.compile(".*Invitation to (\\w+).*");
+    @Then("I delete the invitation to minion {int}")
+    public void i_delete_the_invitation_to_minion(final int i) {
+        final String minion = "minion_"+i;
+        final Optional<WebElement> we =
+            waitForElements(By.className("invitation"))
+                .parallelStream()
+                .filter(e -> {
+                    final Matcher m = INVITATION.matcher(
+                        e.findElement(
+                            By.cssSelector("h3>span:first-of-type")
+                        ).getText()
+                    );
+                    return m.matches() && m.group(1).contentEquals(minion);
+                })
+                .findFirst();
+        assertTrue(we.isPresent());
+        we.get().findElement(By.cssSelector("h3>span:last-of-type")).click();
+        alert(true);
+        pause();
+    }
+    private void clickDeleteEvent(final int i,final boolean accept) {
+        waitForElements(By.className("event"))
+            .get(i)
+            .findElement(By.cssSelector("h4>span.cursor-pointer"))
+            .click();
+        alert(accept);
+        pause();
+    }
+    @Then("I delete event {int}")
+    public void i_delete_event(final int i) {
+        clickDeleteEvent(i,true);
+    }
+    @Then("The minion should see {int} invites")
+    public void the_minion_should_see_invites(final int i) {
+        i_should_see_invites(i);
+    }
+    
+    private void clickDeleteInvite(final int i,final boolean accept) {
+        waitForElements(By.className("invitation"))
+            .get(i)
+            .findElement(By.cssSelector("h3>span:last-of-type"))
+            .click();
+        alert(accept);
+        pause();
+    }
+    @Then("I cancel deleting event {int}")
+    public void i_cancel_deleting_event(final int i) {
+        clickDeleteEvent(i,false);
+    }
+    @Then("I cancel deleting invite {int}")
+    public void i_cancel_deleting_invite(final int i) {
+        clickDeleteInvite(i,false);
+    }
+    
+    @Then("I should see {int} group dates")
+    public void i_should_see_group_dates(final int i) {
+        if(i == 0)
+            assertTrue(
+                waitForElement(By.cssSelector("#content>div:first-of-type>ul"))
+                    .findElements(By.tagName("li"))
+                    .isEmpty()
+            );
+        else
+            assertEquals(
+                i,
+                waitForElements(By.cssSelector("#content>div:first-of-type>ul>li"))
+                    .size()
+            );
+    }
+    
+    @Then("I delete invite {int}")
+    public void i_delete_invite(final int i) {
+        clickDeleteInvite(i,true);
+    }
     
     @After()
     public void after() {
